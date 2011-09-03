77effe1954593d7050627df160c35a5b857720db
Clemens Boos <clemens.boos@gmx.de>
2011-09-03 14:26:33 +0200

    Added station favorite system
    Shorter constant names
    Separate user configuration

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -21,20 +21,27 @@ namespace inetr {
 	HWND MainWindow::stationListBox;
 	HWND MainWindow::statusLabel;
 	HWND MainWindow::stationImage;
+	HWND MainWindow::moreStationListBox;
 
 	list<Language> MainWindow::languages;
 	Language MainWindow::CurrentLanguage;
+	Language *MainWindow::defaultLanguage = NULL;
 
 	HSTREAM MainWindow::currentStream = NULL;
 
 	list<Station> MainWindow::stations;
+	list<Station*> MainWindow::favoriteStations;
 	Station *MainWindow::currentStation = NULL;
+
+	WindowSlideStatus MainWindow::slideStatus = Retracted;
+	int MainWindow::slideOffset = 0;
 
 	int MainWindow::Main(string commandLine, HINSTANCE instance, int showCmd) {
 		MainWindow::instance = instance;
 
 		try {
 			loadConfig();
+			loadUserConfig();
 		} catch (const string &e) {
 			MessageBox(NULL, e.c_str(), "Error", MB_ICONERROR | MB_OK);
 		}
@@ -54,13 +61,19 @@ namespace inetr {
 			DispatchMessage(&msg);
 		}
 
+		try {
+			saveUserConfig();
+		} catch (const string &e) {
+			MessageBox(NULL, e.c_str(), "Error", MB_ICONERROR | MB_OK);
+		}
+
 		return msg.wParam;
 	}
 
 	void MainWindow::createWindow() {
 		WNDCLASSEX wndClass;
 		wndClass.cbSize				= sizeof(WNDCLASSEX);
-		wndClass.style				= 0;
+		wndClass.style				= CS_DBLCLKS;
 		wndClass.lpfnWndProc		= (WNDPROC)(&(MainWindow::wndProc));
 		wndClass.cbClsExtra			= 0;
 		wndClass.cbWndExtra			= 0;
@@ -72,16 +85,16 @@ namespace inetr {
 		wndClass.hCursor			= LoadCursor(NULL, IDC_ARROW);
 		wndClass.hbrBackground		= (HBRUSH)(COLOR_WINDOW + 1);
 		wndClass.lpszMenuName		= NULL;
-		wndClass.lpszClassName		= INTERNETRADIO_MAINWINDOW_CLASSNAME;
+		wndClass.lpszClassName		= INETR_MWND_CLASSNAME;
 
 		if (!RegisterClassEx(&wndClass))
 			throw CurrentLanguage["wndRegFailed"];
 
 		window = CreateWindowEx(WS_EX_CLIENTEDGE,
-			INTERNETRADIO_MAINWINDOW_CLASSNAME, CurrentLanguage["windowTitle"].c_str(),
+			INETR_MWND_CLASSNAME, CurrentLanguage["windowTitle"].c_str(),
 			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
 			CW_USEDEFAULT, CW_USEDEFAULT,
-			INTERNETRADIO_MAINWINDOW_WIDTH, INTERNETRADIO_MAINWINDOW_HEIGHT,
+			INETR_MWND_WIDTH, INETR_MWND_HEIGHT,
 			NULL, NULL, instance, NULL);
 
 		if (window == NULL)
@@ -91,11 +104,11 @@ namespace inetr {
 	void MainWindow::createControls(HWND hwnd) {
 		stationListBox = CreateWindowEx(WS_EX_CLIENTEDGE, "LISTBOX", "",
 			WS_CHILD | WS_VISIBLE | LBS_STANDARD | LBS_SORT | WS_VSCROLL |
-			WS_TABSTOP, INTERNETRADIO_MAINWINDOW_STATIONLIST_POSX,
-			INTERNETRADIO_MAINWINDOW_STATIONLIST_POSY,
-			INTERNETRADIO_MAINWINDOW_STATIONLIST_WIDTH,
-			INTERNETRADIO_MAINWINDOW_STATIONLIST_HEIGHT, hwnd,
-			(HMENU)INTERNETRADIO_MAINWINDOW_STATIONLIST_ID,
+			WS_TABSTOP, INETR_MWND_STATIONLIST_POSX,
+			INETR_MWND_STATIONLIST_POSY,
+			INETR_MWND_STATIONLIST_WIDTH,
+			INETR_MWND_STATIONLIST_HEIGHT, hwnd,
+			(HMENU)INETR_MWND_STATIONLIST_ID,
 			instance, NULL);
 
 		if (stationListBox == NULL)
@@ -106,29 +119,46 @@ namespace inetr {
 			MAKELPARAM(FALSE, 0));
 
 		statusLabel = CreateWindow("STATIC", "", WS_CHILD | WS_VISIBLE |
-			WS_TABSTOP, INTERNETRADIO_MAINWINDOW_STATIONLABEL_POSX,
-			INTERNETRADIO_MAINWINDOW_STATIONLABEL_POSY,
-			INTERNETRADIO_MAINWINDOW_STATIONLABEL_WIDTH,
-			INTERNETRADIO_MAINWINDOW_STATIONLABEL_HEIGHT, hwnd,
-			(HMENU)INTERNETRADIO_MAINWINDOW_STATIONLABEL_ID, instance, NULL);
+			WS_TABSTOP, INETR_MWND_STATUSLABEL_POSX,
+			INETR_MWND_STATUSLABEL_POSY,
+			INETR_MWND_STATUSLABEL_WIDTH,
+			INETR_MWND_STATUSLABEL_HEIGHT, hwnd,
+			(HMENU)INETR_MWND_STATUSLABEL_ID, instance, NULL);
 
 		if (statusLabel == NULL)
 			throw CurrentLanguage["staLblCreFailed"];
 
-		stationImage = CreateWindow("STATIC", "", WS_CHILD | WS_VISIBLE |
-			SS_BITMAP, INTERNETRADIO_MAINWINDOW_STATIONIMAGE_POSX,
-			INTERNETRADIO_MAINWINDOW_STATIONIMAGE_POSY, 0, 0, hwnd,
-			(HMENU)INTERNETRADIO_MAINWINDOW_STATIONIMAGE_ID, instance, NULL);
+		stationImage = CreateWindow("STATIC", "", WS_CHILD |
+			SS_BITMAP, INETR_MWND_STATIONIMAGE_POSX,
+			INETR_MWND_STATIONIMAGE_POSY, 0, 0, hwnd,
+			(HMENU)INETR_MWND_STATIONIMAGE_ID, instance, NULL);
 
 		if (stationImage == NULL)
 			throw CurrentLanguage["staImgCreFailed"];
+
+		moreStationListBox = CreateWindowEx(WS_EX_CLIENTEDGE, "LISTBOX", "",
+			WS_CHILD | LBS_STANDARD | LBS_SORT | WS_VSCROLL |
+			WS_TABSTOP, INETR_MWND_MORESTATIONLIST_POSX,
+			INETR_MWND_MORESTATIONLIST_POSY,
+			INETR_MWND_MORESTATIONLIST_WIDTH,
+			INETR_MWND_MORESTATIONLIST_HEIGHT, hwnd,
+			(HMENU)INETR_MWND_MORESTATIONLIST_ID,
+			instance, NULL);
+
+		if (moreStationListBox == NULL)
+			throw string(CurrentLanguage["staLboxCreFailed"]);
+
+		SendMessage(moreStationListBox, WM_SETFONT, (WPARAM)defaultFont,
+			MAKELPARAM(FALSE, 0));
+
 
 		SendMessage(statusLabel, WM_SETFONT, (WPARAM)defaultFont,
 			MAKELPARAM(FALSE, 0));
 	}
 
 	void MainWindow::initialize(HWND hwnd) {
-		populateListbox();
+		populateStationsListbox();
+		populateMoreStationsListbox();
 
 		BASS_Init(-1, 44100, 0, hwnd, NULL);
 	}
@@ -191,21 +221,21 @@ namespace inetr {
 			languages.push_back(Language(name, strings));
 		}
 
-		Value languageValue = rootValue.get("language", NULL);
-		if (languageValue == NULL || !languageValue.isString())
+		Value defaultLanguageValue = rootValue.get("defaultLanguage", NULL);
+		if (defaultLanguageValue == NULL || !defaultLanguageValue.isString())
 			throw string("Error while parsing config file");
-		string languageStr = languageValue.asString();
+		string strDefaultLanguage = defaultLanguageValue.asString();
 
 		for (list<Language>::iterator it = languages.begin();
 			it != languages.end(); ++it) {
 
-			if (it->Name == languageStr)
-				CurrentLanguage = *it;
+				if (it->Name == strDefaultLanguage)
+					defaultLanguage = &*it;
 		}
 
-		if (CurrentLanguage.Name == "Undefined")
+		if (defaultLanguage == NULL)
 			throw string("Error while parsing config file\n") +
-				string("Unsupported language: ") + languageStr;
+				string("Unsupported language: ") + strDefaultLanguage;
 
 		Value stationList = rootValue.get("stations", NULL);
 		if (stationList == NULL || !stationList.isArray())
@@ -318,12 +348,111 @@ namespace inetr {
 		configFile.close();
 	}
 
-	void MainWindow::populateListbox() {
+	void MainWindow::loadUserConfig() {
+		ifstream configFile;
+		configFile.open("userconfig.json");
+
+		if (configFile.is_open()) {
+			Value rootValue;
+			Reader jsonReader;
+
+			bool successfullyParsed = jsonReader.parse(configFile, rootValue);
+			if (!successfullyParsed)
+				throw string("Couldn't parse user config file\n") +
+				jsonReader.getFormatedErrorMessages();
+
+			Value languageValue = rootValue.get("language", NULL);
+			if (languageValue == NULL || !languageValue.isString())
+				throw string("Error while parsing config file");
+			string languageStr = languageValue.asString();
+
+			for (list<Language>::iterator it = languages.begin();
+				it != languages.end(); ++it) {
+
+					if (it->Name == languageStr)
+						CurrentLanguage = *it;
+			}
+
+			if (CurrentLanguage.Name == "Undefined")
+				throw string("Error while parsing user config file\n") +
+				string("Unsupported language: ") + languageStr;
+
+			Value favoriteStationsValue = rootValue.get("favoriteStations",
+				NULL);
+			if (favoriteStationsValue == NULL ||
+				!favoriteStationsValue.isArray())
+				throw string("Error while parsing config file");
+
+			for (unsigned int i = 0; i < favoriteStationsValue.size(); ++i) {
+				Value favoriteStationValue = favoriteStationsValue[i];
+				if (!favoriteStationValue.isString())
+					throw string("Error while parsing config file");
+				string favoriteStationStr = favoriteStationValue.asString();
+
+				Station *favoriteStation = NULL;
+				for (list<Station>::iterator it = stations.begin();
+					it != stations.end(); ++it) {
+					
+					if (it->Name == favoriteStationStr)
+						favoriteStation = &*it;
+				}
+
+				if (favoriteStation == NULL)
+					throw string("Error while parsing config file\n") +
+						string("Unknown station: ") + favoriteStationStr;
+
+				favoriteStations.push_back(favoriteStation);
+			}
+
+		} else {
+			CurrentLanguage = *defaultLanguage;
+		}
+	}
+
+	void MainWindow::saveUserConfig() {
+		Value rootValue(objectValue);
+
+		rootValue["language"] = Value(CurrentLanguage.Name);
+		rootValue["favoriteStations"] = Value(arrayValue);
+
+		for (list<Station*>::iterator it = favoriteStations.begin();
+			it != favoriteStations.end(); ++it) {
+
+			rootValue["favoriteStations"].append(Value((*it)->Name));
+		}
+
+		StyledWriter jsonWriter;
+
+		string json = jsonWriter.write(rootValue);
+
+		ofstream configFile;
+		configFile.open("userconfig.json", ios::out | ios::trunc);
+
+		if (!configFile.is_open())
+			throw string("Couldn't open user config file");
+
+		configFile << json;
+
+		configFile.close();
+	}
+	
+	void MainWindow::populateStationsListbox() {
+		SendMessage(stationListBox, LB_RESETCONTENT, 0, 0);
+
+		for (list<Station*>::iterator it = favoriteStations.begin();
+			it != favoriteStations.end(); ++it) {
+
+			SendMessage(stationListBox, (UINT)LB_ADDSTRING, (WPARAM)0,
+				(LPARAM)(*it)->Name.c_str());
+		}
+	}
+
+	void MainWindow::populateMoreStationsListbox() {
 		for (list<Station>::iterator it = stations.begin();
 			it != stations.end(); ++it) {
 
-			SendMessage(stationListBox, (UINT)LB_ADDSTRING, (WPARAM)0,
-				(LPARAM)it->Name.c_str());
+				SendMessage(moreStationListBox, (UINT)LB_ADDSTRING, (WPARAM)0,
+					(LPARAM)it->Name.c_str());
 		}
 	}
 
@@ -335,7 +464,7 @@ namespace inetr {
 		if (progress > 75 || !BASS_StreamGetFilePosition(currentStream,
 			BASS_FILEPOS_CONNECTED)) {
 
-				KillTimer(window, INTERNETRADIO_MAINWINDOW_TIMER_BUFFER);
+				KillTimer(window, INETR_MWND_TIMER_BUFFER);
 
 				SetWindowText(statusLabel, CurrentLanguage["connected"].c_str());
 
@@ -354,7 +483,7 @@ namespace inetr {
 
 				BASS_ChannelPlay(currentStream, FALSE);
 
-				SetTimer(window, INTERNETRADIO_MAINWINDOW_TIMER_META, 5000,
+				SetTimer(window, INETR_MWND_TIMER_META, 5000,
 					NULL);
 		} else {
 			stringstream sstreamStatusText;
@@ -368,13 +497,68 @@ namespace inetr {
 		updateMeta();
 	}
 
+	void MainWindow::slideTimer() {
+		int oSlideOffset = slideOffset;
+
+		switch (slideStatus) {
+		case Expanding:
+			slideOffset += INETR_MWND_SLIDE_STEP;
+			if (slideOffset >= INETR_MWND_SLIDE_MAX) {
+				slideOffset = INETR_MWND_SLIDE_MAX;
+				slideStatus = Expanded;
+				KillTimer(window, INETR_MWND_TIMER_SLIDE);
+			}
+			break;
+		case Retracting:
+			slideOffset -= INETR_MWND_SLIDE_STEP;
+			if (slideOffset <= 0) {
+				slideOffset = 0;
+				slideStatus = Retracted;
+				KillTimer(window, INETR_MWND_TIMER_SLIDE);
+			}
+		}
+		
+		RECT wndPos;
+		GetWindowRect(window, &wndPos);
+		int slideOffsetDiff = slideOffset - oSlideOffset;
+
+		MoveWindow(window, wndPos.left - slideOffsetDiff,
+			wndPos.top, INETR_MWND_WIDTH + slideOffset,
+			INETR_MWND_HEIGHT, TRUE);
+
+		SetWindowPos(stationListBox, NULL,
+			INETR_MWND_STATIONLIST_POSX + slideOffset,
+			INETR_MWND_STATIONLIST_POSY, 0, 0, SWP_NOSIZE);
+
+		SetWindowPos(stationImage, NULL,
+			INETR_MWND_STATIONIMAGE_POSX + slideOffset,
+			INETR_MWND_STATIONIMAGE_POSY, 0, 0, SWP_NOSIZE);
+
+		SetWindowPos(statusLabel, NULL,
+			INETR_MWND_STATUSLABEL_POSX + slideOffset,
+			INETR_MWND_STATUSLABEL_POSY, 0, 0, SWP_NOSIZE);
+
+		int moreStationListBoxWidth = slideOffset - 10;
+		if (moreStationListBoxWidth <= 0) {
+			ShowWindow(moreStationListBox, SW_HIDE);
+		} else {
+			SetWindowPos(moreStationListBox, NULL,
+				0, 0, moreStationListBoxWidth,
+				INETR_MWND_MORESTATIONLIST_HEIGHT, SWP_NOMOVE);
+			ShowWindow(moreStationListBox, SW_SHOW);
+		}
+	}
+
 	void CALLBACK MainWindow::metaSync(HSYNC handle, DWORD channel, DWORD data,
 		void *user) {
 
 		updateMeta();
 	}
 
-	void MainWindow::handleListboxClick() {
+	void MainWindow::handleStationsListboxClick() {
+		if (slideStatus != Retracted)
+			return;
+
 		int index = SendMessage(stationListBox, LB_GETCURSEL, 0, 0);
 		int textLength = SendMessage(stationListBox, LB_GETTEXTLEN,
 			(WPARAM)index, 0);
@@ -388,6 +572,7 @@ namespace inetr {
 			
 			if (text == it->Name && &*it != currentStation) {
 				currentStation = &*it;
+				ShowWindow(stationImage, SW_SHOW);
 				SendMessage(stationImage, STM_SETIMAGE, IMAGE_BITMAP,
 					(LPARAM)currentStation->Image);
 				openURL(it->URL);
@@ -395,9 +580,57 @@ namespace inetr {
 		}
 	}
 
+	void MainWindow::handleStationsListboxDblClick() {
+		if (slideStatus != Expanded)
+			return;
+
+		int index = SendMessage(stationListBox, LB_GETCURSEL, 0, 0);
+		int textLength = SendMessage(stationListBox, LB_GETTEXTLEN,
+			(WPARAM)index, 0);
+		char* cText = new char[textLength + 1];
+		SendMessage(stationListBox, LB_GETTEXT, (WPARAM)index, (LPARAM)cText);
+		string text(cText);
+		delete[] cText;
+
+		for (list<Station*>::iterator it = favoriteStations.begin();
+			it != favoriteStations.end(); ) {
+
+			if (text == (*it)->Name) {
+				it = favoriteStations.erase(it);
+			} else {
+				++it;
+			}
+		}
+
+		populateStationsListbox();
+	}
+
+	void MainWindow::handleMoreStationsListboxDblClick() {
+		if (slideStatus != Expanded)
+			return;
+
+		int index = SendMessage(moreStationListBox, LB_GETCURSEL, 0, 0);
+		int textLength = SendMessage(moreStationListBox, LB_GETTEXTLEN,
+			(WPARAM)index, 0);
+		char* cText = new char[textLength + 1];
+		SendMessage(moreStationListBox, LB_GETTEXT, (WPARAM)index,
+			(LPARAM)cText);
+		string text(cText);
+		delete[] cText;
+
+		for (list<Station>::iterator it = stations.begin();
+			it != stations.end(); ++it) {
+			
+			if (text == it->Name)
+				favoriteStations.push_back(&*it);
+		}
+
+		populateStationsListbox();
+	}
+
 	void MainWindow::openURL(string url) {
-		KillTimer(window, INTERNETRADIO_MAINWINDOW_TIMER_BUFFER);
-		KillTimer(window, INTERNETRADIO_MAINWINDOW_TIMER_META);
+		KillTimer(window, INETR_MWND_TIMER_BUFFER);
+		KillTimer(window, INETR_MWND_TIMER_META);
 
 		if (currentStream != NULL) {
 			BASS_ChannelStop(currentStream);
@@ -410,9 +643,24 @@ namespace inetr {
 		currentStream = BASS_StreamCreateURL(url.c_str(), 0, 0, NULL, 0);
 
 		if (currentStream != NULL)
-			SetTimer(window, INTERNETRADIO_MAINWINDOW_TIMER_BUFFER, 50, NULL);
+			SetTimer(window, INETR_MWND_TIMER_BUFFER, 50, NULL);
 		else
 			SetWindowText(statusLabel, CurrentLanguage["connectionError"].c_str());
+	}
+
+	void MainWindow::stop() {
+		if (currentStream != NULL) {
+			BASS_ChannelStop(currentStream);
+			BASS_StreamFree(currentStream);
+		}
+
+		ShowWindow(stationImage, SW_HIDE);
+		SetWindowText(statusLabel, "");
+
+		KillTimer(window, INETR_MWND_TIMER_BUFFER);
+		KillTimer(window, INETR_MWND_TIMER_META);
+
+		currentStation = NULL;
 	}
 
 	void MainWindow::updateMeta() {
@@ -545,26 +793,57 @@ namespace inetr {
 		return string(resA[1]) + " - " + string(resT[1]);
 	}
 
+	void MainWindow::expand() {
+		if (slideStatus != Retracted)
+			return;
+
+		slideStatus = Expanding;
+		SetTimer(window, INETR_MWND_TIMER_SLIDE,
+			INETR_MWND_SLIDE_SPEED, NULL);
+	}
+
+	void MainWindow::retract() {
+		if (slideStatus != Expanded)
+			return;
+
+		slideStatus = Retracting;
+		SetTimer(window, INETR_MWND_TIMER_SLIDE,
+			INETR_MWND_SLIDE_SPEED, NULL);
+	}
+
 	LRESULT CALLBACK MainWindow::wndProc(HWND hwnd, UINT uMsg, WPARAM wParam,
 		LPARAM lParam) {
 		
 		switch (uMsg) {
 		case WM_TIMER:
 			switch (wParam) {
-				case INTERNETRADIO_MAINWINDOW_TIMER_BUFFER:
+				case INETR_MWND_TIMER_BUFFER:
 					bufferTimer();
 					break;
-				case INTERNETRADIO_MAINWINDOW_TIMER_META:
+				case INETR_MWND_TIMER_META:
 					metaTimer();
+					break;
+				case INETR_MWND_TIMER_SLIDE:
+					slideTimer();
 					break;
 			}
 			break;
 		case WM_COMMAND:
 			switch (LOWORD(wParam)) {
-			case INTERNETRADIO_MAINWINDOW_STATIONLIST_ID:
+			case INETR_MWND_STATIONLIST_ID:
 				switch (HIWORD(wParam)) {
 				case LBN_SELCHANGE:
-					handleListboxClick();
+					handleStationsListboxClick();
+					break;
+				case LBN_DBLCLK:
+					handleStationsListboxDblClick();
+					break;
+				}
+				break;
+			case INETR_MWND_MORESTATIONLIST_ID:
+				switch(HIWORD(wParam)) {
+				case LBN_DBLCLK:
+					handleMoreStationsListboxDblClick();
 					break;
 				}
 				break;
@@ -580,6 +859,14 @@ namespace inetr {
 			break;
 		case WM_CTLCOLORSTATIC:
 			return (INT_PTR)GetStockObject(WHITE_BRUSH);
+			break;
+		case WM_LBUTTONDBLCLK:
+			if (slideStatus == Retracted) {
+				stop();
+				expand();
+			} else if (slideStatus == Expanded) {
+				retract();
+			}
 			break;
 		case WM_CLOSE:
 			uninitialize(hwnd);
--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -10,30 +10,43 @@
 #include "Station.hpp"
 #include "Language.hpp"
 
-#define INTERNETRADIO_MAINWINDOW_CLASSNAME "InternetRadio"
-#define INTERNETRADIO_MAINWINDOW_WIDTH 350
-#define INTERNETRADIO_MAINWINDOW_HEIGHT 300
+#define INETR_MWND_CLASSNAME "InternetRadio"
+#define INETR_MWND_WIDTH 350
+#define INETR_MWND_HEIGHT 292
 
-#define INTERNETRADIO_MAINWINDOW_STATIONLIST_ID 101
-#define INTERNETRADIO_MAINWINDOW_STATIONLIST_POSX 10
-#define INTERNETRADIO_MAINWINDOW_STATIONLIST_POSY 10
-#define INTERNETRADIO_MAINWINDOW_STATIONLIST_WIDTH 100
-#define INTERNETRADIO_MAINWINDOW_STATIONLIST_HEIGHT 250
+#define INETR_MWND_STATIONLIST_ID 101
+#define INETR_MWND_STATIONLIST_POSX 10
+#define INETR_MWND_STATIONLIST_POSY 10
+#define INETR_MWND_STATIONLIST_WIDTH 100
+#define INETR_MWND_STATIONLIST_HEIGHT 250
 
-#define INTERNETRADIO_MAINWINDOW_STATIONLABEL_ID 201
-#define INTERNETRADIO_MAINWINDOW_STATIONLABEL_POSX 120
-#define INTERNETRADIO_MAINWINDOW_STATIONLABEL_POSY 215
-#define INTERNETRADIO_MAINWINDOW_STATIONLABEL_WIDTH 200
-#define INTERNETRADIO_MAINWINDOW_STATIONLABEL_HEIGHT 30
+#define INETR_MWND_STATUSLABEL_ID 201
+#define INETR_MWND_STATUSLABEL_POSX 120
+#define INETR_MWND_STATUSLABEL_POSY 215
+#define INETR_MWND_STATUSLABEL_WIDTH 200
+#define INETR_MWND_STATUSLABEL_HEIGHT 30
 
-#define INTERNETRADIO_MAINWINDOW_STATIONIMAGE_ID 301
-#define INTERNETRADIO_MAINWINDOW_STATIONIMAGE_POSX 120
-#define INTERNETRADIO_MAINWINDOW_STATIONIMAGE_POSY 10
+#define INETR_MWND_STATIONIMAGE_ID 301
+#define INETR_MWND_STATIONIMAGE_POSX 120
+#define INETR_MWND_STATIONIMAGE_POSY 10
 
-#define INTERNETRADIO_MAINWINDOW_TIMER_BUFFER 0
-#define INTERNETRADIO_MAINWINDOW_TIMER_META 1
+#define INETR_MWND_MORESTATIONLIST_ID 401
+#define INETR_MWND_MORESTATIONLIST_POSX 10
+#define INETR_MWND_MORESTATIONLIST_POSY 10
+#define INETR_MWND_MORESTATIONLIST_WIDTH 100
+#define INETR_MWND_MORESTATIONLIST_HEIGHT 250
+
+#define INETR_MWND_SLIDE_MAX 110
+#define INETR_MWND_SLIDE_STEP 2
+#define INETR_MWND_SLIDE_SPEED 1
+
+#define INETR_MWND_TIMER_BUFFER 0
+#define INETR_MWND_TIMER_META 1
+#define INETR_MWND_TIMER_SLIDE 2
 
 namespace inetr {
+	enum WindowSlideStatus { Retracted, Expanded, Expanding, Retracting };
+
 	class MainWindow {
 	public:
 		static int Main(std::string commandLine, HINSTANCE instance,
@@ -46,15 +59,22 @@ namespace inetr {
 		static void initialize(HWND hwnd);
 		static void uninitialize(HWND hwnd);
 		static void loadConfig();
-		static void populateListbox();
+		static void loadUserConfig();
+		static void saveUserConfig();
+		static void populateStationsListbox();
+		static void populateMoreStationsListbox();
 
 		static void bufferTimer();
 		static void metaTimer();
+		static void slideTimer();
 		static void CALLBACK metaSync(HSYNC handle, DWORD channel, DWORD data,
 			void *user);
-		static void handleListboxClick();
+		static void handleStationsListboxClick();
+		static void handleStationsListboxDblClick();
+		static void handleMoreStationsListboxDblClick();
 
 		static void openURL(std::string url);
+		static void stop();
 		static void updateMeta();
 		static std::string fetchMeta();
 		static std::string fetchMeta_meta();
@@ -62,6 +82,9 @@ namespace inetr {
 		static std::string fetchMeta_http();
 		static std::string processMeta_regex(std::string meta);
 		static std::string processMeta_regexAT(std::string meta);
+
+		static void expand();
+		static void retract();
 
 		static LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam,
 			LPARAM lParam);
@@ -71,13 +94,19 @@ namespace inetr {
 		static HWND stationListBox;
 		static HWND statusLabel;
 		static HWND stationImage;
+		static HWND moreStationListBox;
 
 		static std::list<Language> languages;
+		static Language *defaultLanguage;
 
 		static HSTREAM currentStream;
 
 		static std::list<Station> stations;
+		static std::list<Station*> favoriteStations;
 		static Station* currentStation;
+
+		static WindowSlideStatus slideStatus;
+		static int slideOffset;
 	};
 }
 
